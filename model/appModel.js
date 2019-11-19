216eb09c8a221b4b854d793115b91b968fa3fe67
Administrator <skyestzhang@163.com>
2019-11-19 14:07:09 +0800

    update app

--- model/appModel.js
+++ model/appModel.js
@@ -28,12 +28,29 @@ const appModel = {
     },
     updateApp(p) {
         const fields = [
-          'id', 'name','layout'
+          'id', 'name','layout',	
+          'top_bg_color',
+          'logo',
+          'app_name',
+          'show_app_name',
+          'side_bg_color',
+          'side_text_color',
+          'side_text_active_color',
+          'app_name_color'
         ]
         const params = fields.map(f => {
             return p[f]
         })
-        return query(`update t_app set name=$2,layout=$3 where id=$1`, params)
+        return query(`update t_app set name=$2,layout=$3,        	
+            top_bg_color=$4,
+            logo=$5,
+            app_name=$6,	
+            show_app_name=$7,
+            side_bg_color=$8,
+            side_text_color=$9,
+            side_text_active_color=$10,
+            app_name_color=$11,
+         where id=$1`, params)
     },
     deleteApp: (p) => {
         return query(`DELETE FROM t_app WHERE id=$1`, [p])
