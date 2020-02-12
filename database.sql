3e406136f0290cd9898289c4d5856e46fa5fbf6b
zillionnn <skyestzhang@163.com>
2020-02-12 12:47:53 +0800

    fix sql

--- database.sql
+++ database.sql
@@ -1,6 +1,15 @@
 CREATE TABLE public.t_app
 (
     id serial,
+    layout text,
+    top_bg_color text,
+    logo text,
+    side_bg_color text,
+       show_app_name BOOLEAN,
+         app_name text,
+         side_text_color text,
+          side_text_active_color text,
+          app_name_color text,
     name text COLLATE pg_catalog."default",
     page_ids text[] COLLATE pg_catalog."default",
     update_time timestamp with time zone DEFAULT now(),
@@ -11,7 +20,7 @@ CREATE TABLE public.t_app
 CREATE TABLE public.t_page
 (
     id serial,
-    page_id integer,
+    page_id serial,
     components text[] COLLATE pg_catalog."default",
     update_time timestamp with time zone DEFAULT now(),
     name text COLLATE pg_catalog."default",
@@ -21,6 +30,7 @@ CREATE TABLE public.t_page
 CREATE TABLE public.t_menu
 (
     id serial,
+    app_id INTEGER,
     menu_id integer,
     name text COLLATE pg_catalog."default",
     CONSTRAINT t_menu_pkey PRIMARY KEY (id)
